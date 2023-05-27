02a8c6dbc941ed008bda9ed4fb9bbc6dfd0a0c5c
amitmaurya <amitmaurya@sorigin.com>
2023-05-27 21:38:18 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import {logo} from "./assets"
+import { CreatePost, Home} from "./pages";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <header className="w-full flex">
+      
+    </header>
+    </BrowserRouter>
+  );
+};
+
+export default App;
