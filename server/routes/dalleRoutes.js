63eeba1c327e4a57eec515415ac83bdade0ca649
AkashMaurya2037 <akashmaurya2037@gmail.com>
2023-06-01 16:56:45 +0530

    Backend Completed

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,13 +7,33 @@ dotenv.config();
 const router = express.Router();
 
 const configuartion = new Configuration({
-    apiKey:process.env.OPENAI_API_KEY,
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-const openai = new OpenAIApi(configuartion)
+const openai = new OpenAIApi(configuartion);
 
-router.route("/").get((req,res)=>{
-    res.send("Hello from Dall-E")
-})
+router.route("/").get((req, res) => {
+  res.send("Hello from Dall-E");
+});
 
-export default router
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error?.response.data.error.message)
+  }
+});
+
+export default router;
