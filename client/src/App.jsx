135d43719f4094e393b98016f72eccc509c7adf5
AkashMaurya2037 <akashmaurya2037@gmail.com>
2023-06-04 12:37:48 +0530

    Small Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import { logo } from "./assets";
+import  {logo}  from "./assets";
 import { CreatePost, Home } from "./pages";
 
 const App = () => {
